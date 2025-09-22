11ee7c8f3af6ac935e5afeecd8283e003c225258
ThanhLeDuc552 <leducthanh552@gmail.com>
2025-09-22 10:56:09 +0700

    Added Jade to Maven dependencies section

---
+++ src/main/java/project/Agent/TrafficPredictAgent1.java
@@ -0,0 +1,16 @@
+package project.Agent;
+import jade.core.Agent;
+import jade.core.AID;
+import org.chocosolver.solver.Model;
+
+public class TrafficPredictAgent1 extends Agent {
+    @Override
+    protected void setup() {
+
+    }
+
+    @Override
+    protected void takeDown() {
+
+    }
+}
